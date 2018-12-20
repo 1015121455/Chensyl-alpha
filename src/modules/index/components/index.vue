8e3af1a1765befac60530cf079ebb630396966e8
yanliang <1015121455@qq.com>
2018-12-20 21:54:10 +0800

    ++

---
+++ src/modules/index/components/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <chen-nav></chen-nav>
+    </div>
+</template>
+<script>
+    import {mapState, mapGetters} from 'vuex'
+    import EventBus from '../EventBus.js'
+    import _ from 'lodash'
+    import '../../../common/css/base.css';
+    import '../../../fonts/iconfont.css';
+    import ChenNav from "./chenNav";
+
+    export default {
+        components: {
+            ChenNav
+        },
+        data() {
+            return {
+
+            }
+        },
+        computed: {},
+        mounted() {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
